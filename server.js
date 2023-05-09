4a79b0bc13c1d9ae2ea2146bcc866320566d4c4e
Jason Ting Hey Chu <tchu17@myseneca.ca>
2023-05-09 00:47:49 -0400

    Update server.js

--- server.js
+++ server.js
@@ -4,18 +4,38 @@ const app = express();
 
 const port = 8080;
 
+const data = {
+    "message" : 'My name is Jason.'
+};
+
 app.listen(port, () => {
     console.log(`Listening on port ${port}.`);
 });
 
+
+var count = 0;
 app.get('/', (req, res) => {
-    console.log('API Fetch Request Received.');
+    console.log('|-----------------------------|');
+    console.log('| API Fetch Request Received. |');
+    console.log('|-----------------------------|');
 
-    const data = {
-        message: 'My name is Jason.'
-    };
 
-    res.send(data.message);
+    res.send(data);
 
-    console.log(`Message: ${data.message}`);
+    console.log(`Message:       ${data.message}`);
+    count++;
+    console.log(`Request Count: ${count}`);
+    console.log('\n');
+});
+
+app.get('/:name', (req, res) => {
+    console.log('********************************');
+    console.log('* API Change Request Received. *');
+    console.log('********************************');
+
+    const name = req.params.name;
+    data.message = `My name is ${name}.`;
+    res.send(data);
+    console.log(`Message:       ${data.message}`);
+    console.log('\n');
 });
